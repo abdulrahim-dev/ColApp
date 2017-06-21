8712416d863e24101706179c35adfccd29cf3aca
Abdul <abdulrahim.ka@luminescent-digital.com>
2017-06-21 11:39:40 +0530

    These class will use for DI and Automapper create mapping. We don't need to give each and every interfaces and mapping models. That configurations will add in  next step

---
+++ ColSopApp/ColSopApp.Web/References/ReferencedAssemblies.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace ColSopApp.Web.References
+{
+    public static class ReferencedAssemblies
+    {
+        public static Assembly Services
+        {
+            get { return Assembly.Load("ColSopApp.Services"); }
+        }
+
+        public static Assembly Repositories
+        {
+            get { return Assembly.Load("ColSopApp.Data"); }
+        }
+
+        public static Assembly Dto
+        {
+            get
+            {
+                return Assembly.Load("ColSopApp.Dto");
+            }
+        }
+
+        public static Assembly Domain
+        {
+            get
+            {
+                return Assembly.Load("ColSopApp.Core");
+            }
+        }
+    }
+}
